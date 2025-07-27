ad31d880004e7e9d3fb4ec1995da447f505b9a79
Anand <anandselvaraj653@gmail.com>
2025-07-27 01:41:26 +0100

    one

---
+++ test_lesson_sample1/src/test_lesson_sample1/employee_controller.java
@@ -0,0 +1,37 @@
+package test_lesson_sample1;
+
+public class employee_controller {
+
+	private String name;
+	private int age;
+	private Float salary;
+	
+	
+	public employee_controller(String name, int age, Float salary) 
+	{
+		super();
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Float getSalary() {
+		return salary;
+	}
+	public void setSalary(Float salary) {
+		this.salary = salary;
+	}
+	
+}
